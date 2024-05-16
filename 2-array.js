c3f4b70b5be070312cab86f1cbdc9ecfc0a79d42
Gustavo1708 <140513756+Gustavo1708@users.noreply.github.com>
2024-05-16 11:05:01 -0300

    Add files via upload

---
+++ 2-array.js
@@ -0,0 +1,12 @@
+var frutas =[];
+
+frutas.push("maçã");
+frutas.push("banana");
+frutas.push("laranja");
+
+console.log('Array após adicionar elementos ao final:', frutas);
+
+frutas.unshift('Pêssego');
+frutas.unshift('Abacaxi');
+
+console.log('Array após adicionar elementos ao inicio:', frutas);
---
+++ 3-array-pop-shift.js
@@ -0,0 +1,10 @@
+var frutas = ['maçã', 'banana', 'laranja', 'pêssego', 'abacaxi'];
+
+var ultimaFruta = frutas.pop();
+
+console.log('Array após remover o último elemento', frutas);
+console.log('última fruta removida:', ultimaFruta);
+
+var primeiraFruta = frutas.unshift();
+console.log('Array após remover o primeria elemento:', frutas);
+console.log('Primeira fruta removida', primeiraFruta);
